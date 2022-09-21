a27ae4c09bcf74617f3953a6cbe024e9f86d977a
Varabyeu <varabyeu.v@yandex.by>
2022-09-21 17:41:59 +0300

    fix some bugs

--- bakery_project/products/urls.py
+++ bakery_project/products/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from .views import ProductListView, ProductDetailView
 
+app_name = 'products'
 
 urlpatterns = [
     path('products/', ProductListView.as_view(), name='products'),
